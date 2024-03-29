5df8212ad795960944fd48a01df1d955292369fd
afobaje <ikukoyidave@gmail.com>
2024-03-29 09:53:34 +0100

    removed line

---
+++ dist/Routes/Route.js
@@ -0,0 +1,17 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = require("express");
+const Controller_1 = require("../Controller");
+const router = (0, express_1.Router)();
+router.get('/getallusers', Controller_1.getAllUsers);
+router.get('/:username', Controller_1.getUserWithUsername);
+router.post('/createuser', Controller_1.createUser);
+router.post('/getusername/:id', Controller_1.registerUsername);
+router.put('/user/:id', Controller_1.updateUserProfile);
+router.post('/projects/:id', Controller_1.updateUserProjects);
+router.post('/education/:id', Controller_1.updateUserEducation);
+router.post('/work/:id', Controller_1.updateUserWorkExperience);
+router.post('/volunteer/:id', Controller_1.updateUserVolunteerExperience);
+router.post('/award/:id', Controller_1.updateUserAward);
+exports.default = router;
+//# sourceMappingURL=Route.js.map
