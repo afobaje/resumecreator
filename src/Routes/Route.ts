fab45324d2dd47c5d1b3399f9ad0a00779910df2
afobaje <ikukoyidave@gmail.com>
2024-03-29 09:44:04 +0100

    worked on error

--- src/Routes/Route.ts
+++ src/Routes/Route.ts
@@ -2,12 +2,10 @@ import { Router } from "express";
 import {
     createPdf, createUser, getAllUsers, updateUserProfile, updateUserProjects, updateUserEducation, updateUserWorkExperience, updateUserVolunteerExperience, updateUserAward, getUserWithUsername, registerUsername,
 } from "../Controller";
-import UserDetails from './../../userFiles.json'
+
 const router = Router()
 
-router.get('/checklist', (req, res) => {
-    res.send(UserDetails)
-})
+
 
 router.get('/getallusers', getAllUsers)
 router.get('/:username', getUserWithUsername)
